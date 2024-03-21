43e5702ced3497cc06b55b1080b890ac92897ef0
Ashwani <ashwanisanuraj@gmail.com>
2024-03-21 23:42:54 +0530

    version 2.1 -Retrieved data from firestore and displayed them on the profile page

--- app/src/main/java/com/xero/myapplication/activity/UserProfileActivity.kt
+++ app/src/main/java/com/xero/myapplication/activity/UserProfileActivity.kt
@@ -1,23 +1,45 @@
 package com.xero.myapplication.activity
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.xero.myapplication.R
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.xero.myapplication.databinding.ActivityUserProfileBinding
 
 class UserProfileActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityUserProfileBinding
+    private lateinit var binding: ActivityUserProfileBinding
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
-
-
-
         setContentView(binding.root)
+
+        preferences = this.getSharedPreferences("user", MODE_PRIVATE)
+
+        loadUserInfo(preferences.getString("number", "")!!)
+
+        binding.goToEditProfile.setOnClickListener{
+            startActivity(Intent(this@UserProfileActivity, EditUserProfileActivity::class.java))
+        }
+
+    }
+
+    private fun loadUserInfo(phoneNumber: String) {
+
+        Firebase.firestore.collection("users").document(phoneNumber)
+            .get()
+            .addOnSuccessListener { document ->
+                if(document.exists()){
+                    val userName = document.getString("userName")
+                    binding.textView15.text = "Name: $userName"
+
+                    val userPhone = document.getString("userPhone")
+                    binding.textView16.text = "Phone No: $userPhone"
+
+                }
+            }
     }
 }
